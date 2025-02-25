5e1b381cadc69807e0c185de9ba88378c4dc9a54
吴志勇 <wuzhiyong@wuzhiyongdeMacBook-Pro.local>
2025-02-25 23:13:42 +0800

    Initial Commit

---
+++ AutoChat/AutoChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoChatApp.swift
+//  AutoChat
+//
+//  Created by 吴志勇 on 2025/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoChatTests/AutoChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoChatTests.swift
+//  AutoChatTests
+//
+//  Created by 吴志勇 on 2025/2/25.
+//
+
+import Testing
+@testable import AutoChat
+
+struct AutoChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
